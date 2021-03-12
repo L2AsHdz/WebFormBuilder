626245f605bfa9ef7f6c7daff36a8cec7e37b704
L2AsHdz <leonidas0197@gmail.com>
2021-03-11 23:49:18 -0600

    Producciones para agregar componente agregadas

--- AppCliente/src/main/java/com/l2ashdz/appcliente/analizadores/sintactico/sym.java
+++ AppCliente/src/main/java/com/l2ashdz/appcliente/analizadores/sintactico/sym.java
@@ -8,45 +8,72 @@ package com.l2ashdz.appcliente.analizadores.sintactico;
 /** CUP generated class containing symbol constants. */
 public class sym {
   /* terminals */
-  public static final int PARAM_NEW_USER = 20;
-  public static final int PARAM_ID = 22;
-  public static final int NEW_FORM = 12;
-  public static final int GREATER_THAN = 28;
-  public static final int IDENTIFICADOR = 38;
+  public static final int PARAM_NEW_USER = 18;
+  public static final int PARAM_ID = 23;
+  public static final int EDIT_COMPONENT = 30;
+  public static final int PARAM_ALINEACION = 36;
+  public static final int PARAM_TEXTO_VISIBLE = 35;
+  public static final int CLASS_RADIO = 45;
+  public static final int PARAM_CLASE = 33;
+  public static final int IZQUIERDA = 51;
+  public static final int NEW_FORM = 20;
+  public static final int CLASS_CHECKBOX = 44;
+  public static final int GREATER_THAN = 55;
+  public static final int CLASS_COMBO = 48;
+  public static final int IDENTIFICADOR = 65;
   public static final int END_REQUEST = 4;
-  public static final int PARAM_TEMA = 25;
-  public static final int PARAM_OLD_USER = 19;
-  public static final int COMMA = 32;
+  public static final int PARAM_TEMA = 26;
+  public static final int PARAM_OLD_USER = 17;
+  public static final int PARAM_NOMBRE_CAMPO = 31;
+  public static final int COMMA = 59;
+  public static final int CENTRO = 50;
   public static final int END_REQUESTS = 5;
-  public static final int CREATE_USER = 9;
+  public static final int PARAM_FORMULARIO = 32;
+  public static final int CLASS_CAMPO_TEXTO = 42;
+  public static final int PARAM_URL = 41;
+  public static final int CREATE_USER = 10;
   public static final int EOF = 0;
-  public static final int PARAM_FECHA_CREACION = 17;
-  public static final int PARAM_TITULO_FORM = 23;
-  public static final int PARAM_NOMBRE_FORM = 24;
-  public static final int DELETE_USER = 10;
+  public static final int PARAM_FECHA_CREACION = 15;
+  public static final int CLASS_BOTON = 49;
+  public static final int PARAM_TITULO_FORM = 24;
+  public static final int PARAM_NOMBRE_FORM = 25;
+  public static final int DELETE_USER = 11;
   public static final int error = 1;
-  public static final int QUOTE_MARK = 30;
-  public static final int PARAM_PASSWORD = 16;
-  public static final int OPEN_BRACE = 33;
-  public static final int CLOSE_BRACKET = 36;
-  public static final int OPEN_BRACKET = 35;
-  public static final int PARAM_USUARIO_CREACION = 26;
-  public static final int DELETE_FORM = 13;
+  public static final int QUOTE_MARK = 57;
+  public static final int COMPONENT_PARAMS = 8;
+  public static final int PARAM_PASSWORD = 14;
+  public static final int OPEN_BRACE = 60;
+  public static final int CLASS_AREA_TEXTO = 43;
+  public static final int PARAM_FILAS = 39;
+  public static final int CLOSE_BRACKET = 63;
+  public static final int OPEN_BRACKET = 62;
+  public static final int CLASS_IMAGEN = 47;
+  public static final int PARAM_USUARIO_CREACION = 27;
+  public static final int DELETE_FORM = 21;
   public static final int USER_CREDENTIALS = 6;
+  public static final int PARAM_INDICE = 34;
   public static final int START_REQUEST = 2;
-  public static final int COLON = 31;
-  public static final int PARAM_FECHA_MODIFICACION = 18;
+  public static final int COLON = 58;
+  public static final int PARAM_FECHA_MODIFICACION = 16;
   public static final int START_REQUESTS = 3;
-  public static final int MODIFY_USER = 11;
-  public static final int LOGIN = 8;
+  public static final int MODIFY_USER = 12;
+  public static final int DERECHA = 52;
+  public static final int DELETE_COMPONENT = 29;
+  public static final int LOGIN = 9;
+  public static final int CLASS_FICHERO = 46;
+  public static final int PARAM_COLUMNAS = 40;
   public static final int FORM_PARAMS = 7;
-  public static final int MODIFY_FORM = 14;
-  public static final int CLOSE_BRACE = 34;
-  public static final int VALUE = 37;
-  public static final int PARAM_NEW_PASSWORD = 21;
-  public static final int EXCLAMATION_MARK = 29;
-  public static final int LESS_THAN = 27;
-  public static final int PARAM_USUARIO = 15;
+  public static final int NEW_COMPONENT = 28;
+  public static final int MODIFY_FORM = 22;
+  public static final int CLOSE_BRACE = 61;
+  public static final int VALUE = 64;
+  public static final int PARAM_NEW_PASSWORD = 19;
+  public static final int EXCLAMATION_MARK = 56;
+  public static final int PARAM_OPCIONES = 38;
+  public static final int PARAM_REQUERIDO = 37;
+  public static final int JUSTIFICAR = 53;
+  public static final int LESS_THAN = 54;
+  public static final int PARAM_USUARIO = 13;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
@@ -56,13 +83,11 @@ public class sym {
   "END_REQUESTS",
   "USER_CREDENTIALS",
   "FORM_PARAMS",
+  "COMPONENT_PARAMS",
   "LOGIN",
   "CREATE_USER",
   "DELETE_USER",
   "MODIFY_USER",
-  "NEW_FORM",
-  "DELETE_FORM",
-  "MODIFY_FORM",
   "PARAM_USUARIO",
   "PARAM_PASSWORD",
   "PARAM_FECHA_CREACION",
@@ -70,11 +95,40 @@ public class sym {
   "PARAM_OLD_USER",
   "PARAM_NEW_USER",
   "PARAM_NEW_PASSWORD",
+  "NEW_FORM",
+  "DELETE_FORM",
+  "MODIFY_FORM",
   "PARAM_ID",
   "PARAM_TITULO_FORM",
   "PARAM_NOMBRE_FORM",
   "PARAM_TEMA",
   "PARAM_USUARIO_CREACION",
+  "NEW_COMPONENT",
+  "DELETE_COMPONENT",
+  "EDIT_COMPONENT",
+  "PARAM_NOMBRE_CAMPO",
+  "PARAM_FORMULARIO",
+  "PARAM_CLASE",
+  "PARAM_INDICE",
+  "PARAM_TEXTO_VISIBLE",
+  "PARAM_ALINEACION",
+  "PARAM_REQUERIDO",
+  "PARAM_OPCIONES",
+  "PARAM_FILAS",
+  "PARAM_COLUMNAS",
+  "PARAM_URL",
+  "CLASS_CAMPO_TEXTO",
+  "CLASS_AREA_TEXTO",
+  "CLASS_CHECKBOX",
+  "CLASS_RADIO",
+  "CLASS_FICHERO",
+  "CLASS_IMAGEN",
+  "CLASS_COMBO",
+  "CLASS_BOTON",
+  "CENTRO",
+  "IZQUIERDA",
+  "DERECHA",
+  "JUSTIFICAR",
   "LESS_THAN",
   "GREATER_THAN",
   "EXCLAMATION_MARK",
