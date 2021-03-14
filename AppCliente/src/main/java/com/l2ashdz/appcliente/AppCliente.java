b5eb23b2282f5c076a51a6a0501ddfc85e6c9b00
L2AsHdz <leonidas0197@gmail.com>
2021-03-14 11:21:43 -0600

    Interfaz preeliminar del editor de texto

--- AppCliente/src/main/java/com/l2ashdz/appcliente/AppCliente.java
+++ AppCliente/src/main/java/com/l2ashdz/appcliente/AppCliente.java
@@ -2,6 +2,7 @@ package com.l2ashdz.appcliente;
 
 import com.l2ashdz.appcliente.analizadores.lexico.Lexer;
 import com.l2ashdz.appcliente.analizadores.sintactico.Parser;
+import com.l2ashdz.appcliente.view.EditorTexto;
 import java.io.StringReader;
 
 /**
@@ -11,22 +12,8 @@ import java.io.StringReader;
 public class AppCliente {
 
     public static void main(String[] args) {
-        
-        StringReader str = new StringReader("""
-                                            graficar circulo ( 25 + 12, 12, 15/3, rojo)
-                                            graficar circulo ( 25 + 12, 12, 15/3, azul)
-                                            animar objeto anterior (69/3,2*5,curva)
-                                            graficar cuadrado ( 12*3, 15+1, (15-3) / 4, verde)
-                                            animar objeto anterior (69/3,2*5,curva)
-                                            graficar rectangulo ( 12 * 3 + 2, 15, 4 / 4, 25, negro)
-                                            animar objeto anterior (69/3,2*5,curva)
-                                            graficar linea ( 12 *3 + 2, 15, 4 / 4, 50 * 1, amarillo)
-                                            graficar linea ( 12 *3 + 2, 15, 4 / 4, 50 * 1, cafe)
-                                            animar objeto anterior (69/3,2*5,curva)
-                                            graficar poligono (1,     2,3,((25+(35)-62+6)*1)+1,5,rojo)
-                                            animar objeto anterior (69/3,2*5,linea)""");
-
-        Lexer lex = new Lexer(str);
-        Parser parser = new Parser(lex);
+        EditorTexto et = new EditorTexto();
+        et.setLocationRelativeTo(null);
+        et.setVisible(true);
     }
 }
