409fa37900f7999fe7db7ac7ad3909939a70cdde
L2AsHdz <leonidas0197@gmail.com>
2021-03-19 14:48:33 -0600

    Envio de datos al servidor funcional

--- AppServer/src/main/java/web/RequestReaderServlet.java
+++ AppServer/src/main/java/web/RequestReaderServlet.java
@@ -1,13 +1,19 @@
 package web;
 
+import analizadores.lexico.Lexer;
+import analizadores.sintactico.Parser;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.errores.ErrorAnalisis;
+import model.solicitudes.Solicitud;
 
 /**
  * @date 18/03/2021
@@ -28,20 +34,45 @@ public class RequestReaderServlet extends HttpServlet {
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
+
         BufferedReader reader = request.getReader();
-        String s;
+        /*String s;
         while ((s = reader.readLine()) != null) {
             System.out.println(s);
+        }*/
+
+        Lexer lex = null;
+        Parser parser = null;
+
+        try {
+            lex = new Lexer(reader);
+            parser = new Parser(lex);
+            parser.parse();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
         }
-        
+
+        List<ErrorAnalisis> errores = new ArrayList();
+
+        errores.addAll(lex.getErrores());
+        errores.addAll(parser.getErrores());
+
+        List<Solicitud> solicitudes = parser.getSolicitudes();
+
         response.setContentType("text/plain");
         try (PrintWriter out = response.getWriter()) {
-            out.print("A dormir perro!!!");
-            out.print("Holi");
-        }
+
+            if (errores.isEmpty()) {
+                solicitudes.forEach(s -> {
+                    out.println("\nSolicitud tipo: " + s.getTipo());
+                    s.getParametros().forEach(param -> out.println("\t" + param.getName() + " : " + param.getValue()));
+                });
+            } else {
+                errores.forEach(e -> out.println(e.getLexema() + "- " + e.getDescripcion() + " Linea: " + e.getLinea() + " Columna: " + e.getColumna()));
+            }
         
+        }
+
     }
-    
-    
+
 }
--- AppServer/src/main/webapp/index.jsp
+++ AppServer/src/main/webapp/index.jsp
@@ -4,4 +4,14 @@
     Author     : asael
 --%>
 
-<meta http-equiv="refresh" content="0;url=${pageContext.request.contextPath}/requestReader">
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
