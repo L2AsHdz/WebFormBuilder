71a3d591882b9eedc6378c8a71f9f6b0fd95a7e9
L2AsHdz <leonidas0197@gmail.com>
2021-03-24 03:06:22 -0600

    Optimizacion a metodos de clase UserBuild

--- AppServer/src/main/java/aux/user/UserBuilder.java
+++ AppServer/src/main/java/aux/user/UserBuilder.java
@@ -14,65 +14,62 @@ import model.solicitudes.Solicitud;
 public class UserBuilder {
 
     private final Solicitud solicitud;
-    private Usuario usuario;
+    private Usuario user;
 
     public UserBuilder(Solicitud s) {
         this.solicitud = s;
     }
 
     public Usuario build() {
-        String nameUser = "";
-        String password = "";
-        String fecha = "";
+        user = new Usuario();
 
-        for (Parametro p : solicitud.getParametros()) {
+        solicitud.getParametros().forEach(p -> {
             String name = p.getName();
             if (name.contains("\"USUARIO\"")) {
-                nameUser = p.getValue().replaceAll("\\s", "").replace("\"", "");
+                user.setNombre(getValue(p.getValue()));
             } else if (name.contains("PASSWORD")) {
-                password = p.getValue().replaceAll("\\s", "").replace("\"", "");
+                user.setPassword(getValue(p.getValue()));
             } else if (name.contains("FECHA_CREACION")) {
-                fecha = p.getValue().replaceAll("\\s", "").replace("\"", "");
+                user.setFechaCreacion(getValue(p.getValue()));
             }
-        }
-        usuario = new Usuario(nameUser, password, fecha);
-        if (usuario.getFechaCreacion().isEmpty()) {
-            usuario.setFechaCreacion(LocalDate.now().toString());
+        });
+        
+        if (user.getFechaCreacion() == null) {
+            user.setFechaCreacion(LocalDate.now().toString());
         }
 
-        return usuario;
+        return user;
     }
     
     public Usuario buildOld() {
-        String nameUser = "";
+        user = new Usuario();
         
-        for (Parametro p : solicitud.getParametros()) {
+        solicitud.getParametros().forEach(p -> {
             if (p.getName().contains("USUARIO_ANTIGUO")) {
-                nameUser = p.getValue().replace("\"", "").replaceAll("\\s", "");
+                user.setNombre(getValue(p.getValue()));
             }
-        }
-        usuario = new Usuario();
-        usuario.setNombre(nameUser);
+        });
         
-        return usuario;
+        return user;
     }
     
     public Usuario buildNew() {
-        usuario = new Usuario();
+        user = new Usuario();
+        
         solicitud.getParametros().forEach(p -> {
             if (p.getName().contains("USUARIO_NUEVO")) {
-                usuario.setNombre(getValue(p.getValue()));
+                user.setNombre(getValue(p.getValue()));
             } else if (p.getName().contains("NUEVO_PASSWORD")) {
-                usuario.setPassword(getValue(p.getValue()));
+                user.setPassword(getValue(p.getValue()));
             } else if (p.getName().contains("MODIFICACION")) {
-                usuario.setFechaCreacion(getValue(p.getValue()));
+                user.setFechaCreacion(getValue(p.getValue()));
             }
         });
-        if (usuario.getFechaCreacion() == null) {
-            usuario.setFechaCreacion(LocalDate.now().toString());
+        if (user.getFechaCreacion() == null) {
+            user.setFechaCreacion(LocalDate.now().toString());
         }
         
-        return usuario;
+        return user;
     }
     
     private String getValue(String s) {
